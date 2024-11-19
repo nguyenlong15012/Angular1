5987334c63907b2f1d509a887c5ce813cec46ecc
Long NB <nguyenlong232253@gmail.com>
2024-11-19 10:41:26 +0700

    Com

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorldComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
